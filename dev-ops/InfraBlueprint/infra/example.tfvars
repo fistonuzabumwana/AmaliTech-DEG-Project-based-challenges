2490fbba6d913f9c2c5434d49f4be531b17a33e0
fistonuzabumwana <youfistalline11@gmail.com>
2026-04-24 23:36:58 +0200

    Add networking and compute resources (VPC, subnets, EC2, security group)

---
+++ dev-ops/InfraBlueprint/infra/example.tfvars
@@ -0,0 +1,7 @@
+aws_region       = "eu-north-1"
+allowed_ssh_cidr = "102.22.184.166/32"
+
+db_username = "admin"
+db_password = "Godisgood008008@"
+
+s3_bucket_name = "vela-assets-fiston-98765"
--- dev-ops/InfraBlueprint/infra/main.tf
+++ dev-ops/InfraBlueprint/infra/main.tf
@@ -19,3 +19,171 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name    = "vela-vpc"
+    Project = "vela-payments"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name    = "vela-igw"
+    Project = "vela-payments"
+  }
+}
+
+# Public Subnet 1
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-subnet-1"
+    Project = "vela-payments"
+  }
+}
+
+# Public Subnet 2
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.aws_region}b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-subnet-2"
+    Project = "vela-payments"
+  }
+}
+
+# Private Subnet 1
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "${var.aws_region}a"
+
+  tags = {
+    Name    = "private-subnet-1"
+    Project = "vela-payments"
+  }
+}
+
+# Private Subnet 2
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "${var.aws_region}b"
+
+  tags = {
+    Name    = "private-subnet-2"
+    Project = "vela-payments"
+  }
+}
+
+
+# Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name    = "public-rt"
+    Project = "vela-payments"
+  }
+}
+
+resource "aws_route_table_association" "public_1_assoc" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_2_assoc" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+
+# Web Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow web and SSH traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ssh_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "web-sg"
+    Project = "vela-payments"
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*"]
+  }
+
+  owners = ["amazon"]
+}
+
+# EC2 Instance
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.public_1.id
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name    = "vela-web"
+    Project = "vela-payments"
+  }
+}
