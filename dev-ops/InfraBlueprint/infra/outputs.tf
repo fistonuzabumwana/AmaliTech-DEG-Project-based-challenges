783eae388dc0de93c2cb924bfc41df5e6df894ba
fistonuzabumwana <youfistalline11@gmail.com>
2026-04-25 01:12:46 +0200

    Add Terraform outputs (EC2 IP, RDS endpoint, S3 bucket)

--- dev-ops/InfraBlueprint/infra/outputs.tf
+++ dev-ops/InfraBlueprint/infra/outputs.tf
@@ -1,16 +1,14 @@
-# Add your outputs here after creating the resources.
+output "ec2_public_ip" {
+  description = "Public IP of EC2 instance"
+  value       = aws_instance.web.public_ip
+}
 
-# output "ec2_public_ip" {
-#   description = "Public IP address of the EC2 instance."
-#   value       = aws_instance.web.public_ip
-# }
+output "rds_endpoint" {
+  description = "RDS database endpoint"
+  value       = aws_db_instance.db.endpoint
+}
 
-# output "rds_endpoint" {
-#   description = "Connection endpoint for the RDS instance."
-#   value       = aws_db_instance.main.endpoint
-# }
-
-# output "s3_bucket_name" {
-#   description = "Name of the S3 static assets bucket."
-#   value       = aws_s3_bucket.assets.bucket
-# }
+output "s3_bucket_name" {
+  description = "S3 bucket name"
+  value       = aws_s3_bucket.assets.bucket
+}
