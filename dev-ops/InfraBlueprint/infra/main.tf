da27a37c05d08d76815fb7fcab354eb75af1f513
fistonuzabumwana <youfistalline11@gmail.com>
2026-04-25 02:22:39 +0200

    Complete InfraBlueprint Terraform infrastructure

--- dev-ops/InfraBlueprint/infra/main.tf
+++ dev-ops/InfraBlueprint/infra/main.tf
@@ -174,7 +174,7 @@ data "aws_ami" "amazon_linux" {
 # EC2 Instance
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   subnet_id = aws_subnet.public_1.id
 
