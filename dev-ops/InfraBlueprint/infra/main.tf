5ee70ad8700439e2449e23aa6ba632a3aab642c2
fistonuzabumwana <youfistalline11@gmail.com>
2026-04-25 03:06:18 +0200

    Add RDS final snapshot protection (bonus)

--- dev-ops/InfraBlueprint/infra/main.tf
+++ dev-ops/InfraBlueprint/infra/main.tf
@@ -247,7 +247,9 @@ resource "aws_db_instance" "db" {
   vpc_security_group_ids = [aws_security_group.db_sg.id]
 
   publicly_accessible = false
-  skip_final_snapshot = true
+  
+  skip_final_snapshot = false
+  final_snapshot_identifier = "vela-db-final-snapshot"
 
   tags = {
     Name    = "vela-db"
